5a2a9930650486c107c40974975526cf3edd5cf9
SamstyleGhost <rohancrrm@gmail.com>
2024-12-11 17:27:12 +0530

    User local storage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,25 @@
+import { useEffect } from "react"
+
+import { v4 as uuidV4} from "uuid";
+import { useUserContext } from "./context";
+
 function App() {
+  
+  const { setCustomerID } = useUserContext();
+  
+  // I am making use of localStorage as a database for small use.
+  // When the user enters the first time, a new uuid will be generated and set. Next time, the same id will be used for the user.
+  // Wont win any awards, but gets the job done right now
+  useEffect(() => {
+    const customer_id = localStorage.getItem("assignment_customer_id");
+    setCustomerID(customer_id);
+    if(!customer_id) {
+      const cid = uuidV4();
+      localStorage.setItem("assignment_customer_id", cid);
+      localStorage.setItem("assignment_customer_cart", "");
+      setCustomerID(cid);
+    }
+  }, [setCustomerID])
 
   return (
     <div>Hello</div>
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router'
+import { UserContextProvider } from './context/index.tsx'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <UserContextProvider>
+        <App />
+      </UserContextProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
